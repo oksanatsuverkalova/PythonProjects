4ce55a6cfdc2910831a274e28106db10a22741bb
Оксана Цуверкалова <74481811+oksanatsuverkalova@users.noreply.github.com>
2021-03-17 16:48:24 +0300

    Применение функций min max sum

---
+++ min_max_sum.py
@@ -0,0 +1,9 @@
+numbers = []
+
+for i in range(3):
+    number = int(input('Пожалуйста, введите число: '))
+    numbers.append(number)
+
+print(max(numbers))
+print(min(numbers))
+print(sum(numbers))
